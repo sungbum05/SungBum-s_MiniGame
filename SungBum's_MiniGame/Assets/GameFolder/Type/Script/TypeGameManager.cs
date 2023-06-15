137758925c7a86f826341c45e3db89f6fbc875fd
sungbum05 <sblim05@naver.com>
2023-06-15 10:52:19 +0900

    TypeGameScene_AddQuestionData

--- SungBum's_MiniGame/Assets/GameFolder/Type/Script/TypeGameManager.cs
+++ SungBum's_MiniGame/Assets/GameFolder/Type/Script/TypeGameManager.cs
@@ -20,11 +20,29 @@ public class TypeGameManager : MonoBehaviour
     }
     [SerializeField]
     private float MaxGauge = 100;
+    [SerializeField]
+    private float DecreaseSpeed;
+    [SerializeField]
+    private string QuestionStr;
+    [SerializeField]
+    private string InputStr;
 
-    [Header("외부 변수")]
+    [Header("외부 변수-컴포넌트")]
     public Image GaugeImage;
     public GameObject StartPanel;
 
+    [Header("외부 변수-시스템")]
+    public InputSystem InputSystem;
+    public QuestionSystem QuestionSystem;
+
+    private void Update()
+    {
+        if(DecreaseSpeed > 0)
+        {
+            CurrentGauge -= DecreaseSpeed * Time.deltaTime;
+            GaugeImage.fillAmount = CurrentGauge / MaxGauge;
+        }
+    }
 
     /// <summary>
     /// Game Start Button Function
@@ -34,5 +52,8 @@ public class TypeGameManager : MonoBehaviour
         StartPanel.SetActive(false);
         MaxGauge = 100;
         CurrentGauge = MaxGauge;
+        DecreaseSpeed = 2.0f;
+
+        InputSystem.GameStart(this);
     }
 }
